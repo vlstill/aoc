253faa766e4642add41da53957e5cbbbdb6a8792
Vladimir Still <git@vstill.eu>
2020-12-05 10:15:14 +0100

    Polish task 05

--- T05.hs
+++ T05.hs
@@ -1,10 +1,9 @@
-{-# LANGUAGE UnicodeSyntax, TypeApplications, NamedFieldPuns #-}
+{-# LANGUAGE UnicodeSyntax, TypeApplications #-}
 
 module T05 where
 
 import Prelude.Unicode
 
-data Coord = Coord { row ∷ Integer, col ∷ Integer } deriving ( Show, Eq )
 data Direction = F | B | L | R deriving ( Show, Eq, Read, Enum )
 
 parseTickets :: String → [[Direction]]
@@ -15,7 +14,6 @@ ticketIDs = map (foldl accDigits 0)
   where
     accDigits acc dig = acc * 2 + fromIntegral (fromEnum dig) `mod` 2
 
-
 main ∷ IO ()
 main = do
     tickets ← parseTickets <$> getContents
@@ -24,3 +22,4 @@ main = do
     print . only . filter (\x → x `notElem` ids ∧ (x + 1) `elem` ids ∧ (x - 1) `elem` ids) $ [0 .. 128 * 8]
   where
     only [x] = x
+    only xs  = error $ "Found more than one element: " <> show xs
