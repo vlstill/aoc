cc26a7c4a0203dc2263a596140ee3c92e40d5652
Vladimir Still <git@vstill.eu>
2020-12-12 12:27:09 +0100

    Second part

--- T09.hs
+++ T09.hs
@@ -1,16 +1,14 @@
-{-# LANGUAGE UnicodeSyntax, TypeApplications, TemplateHaskell, ViewPatterns, ScopedTypeVariables, KindSignatures, PolyKinds, DataKinds #-}
+{-# LANGUAGE UnicodeSyntax, TypeApplications, TemplateHaskell, ViewPatterns, ScopedTypeVariables, PolyKinds, DataKinds, AllowAmbiguousTypes #-}
 
 module T09 where
 
-import Utils
-import Indexable
-
-import Data.Kind ( Type )
 import GHC.TypeLits
-import Control.Lens
 import Data.Default.Class
 import Data.Proxy
+import Data.Maybe
+import Control.Lens
 import Control.Monad.State
+import Control.Arrow
 
 import Prelude.Unicode
 
@@ -23,8 +21,8 @@ instance Default (XMAS β) where
 testIn ∷ [Int]
 testIn = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
 
-firstError ∷ ∀β. KnownNat β ⇒ XMAS β → [Int] → Maybe Int
-firstError buf xs = evalState (go xs) buf
+firstError ∷ ∀β. KnownNat β ⇒ [Int] → Maybe Int
+firstError xs0 = evalState (go xs0) (def @(XMAS β))
   where
     go ∷ [Int] → State (XMAS β) (Maybe Int)
     go [] = pure Nothing
@@ -38,7 +36,18 @@ firstError buf xs = evalState (go xs) buf
 
     size = fromInteger $ natVal @β Proxy
 
+continuousSubrangeWithSum ∷ ∀α. (Ord α, Num α, Show α) ⇒ [α] → α → Maybe [α]
+continuousSubrangeWithSum xss0 s = go [] xss0
+  where
+    go candidates@(sum → currentSum) xss
+      | currentSum ≡ s, length candidates ≥ 2 = Just $ reverse candidates
+      | currentSum < s, (x:xs) ← xss          = go (x : candidates) xs
+      | currentSum > s                        = go (init candidates) xss
+      | otherwise                             = Nothing
+
 main ∷ IO ()
 main = do
     numbers ← map (read @Int) . lines <$> getContents
-    print $ firstError (def @(XMAS 25)) numbers
+    let Just erNum = firstError @25 numbers
+    print erNum
+    print . fromJust $ uncurry (+) . (minimum &&& maximum) <$> continuousSubrangeWithSum numbers erNum
