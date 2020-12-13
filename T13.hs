d87e45d272c78df2d05b4c5f6dee97f97a5c4e39
Vladimir Still <git@vstill.eu>
2020-12-13 09:54:35 +0100

    Second part

--- T13.hs
+++ T13.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE UnicodeSyntax, TypeApplications #-}
+{-# LANGUAGE UnicodeSyntax, TypeApplications, ScopedTypeVariables #-}
 
 module T13 where
 
@@ -11,11 +11,34 @@ import Data.Map.Strict ( Map, singleton, insert, toAscList )
 import Data.Function ( on )
 import Control.Monad.State.Strict
 import Control.Arrow
+import Control.Exception ( assert )
 
+data ModEq ι = ModEq { val ∷ ι, modulo ∷ ι } deriving ( Show, Eq )
+
+solve ∷ ∀ι. Integral ι ⇒ [ModEq ι] → ι
+solve eqs = (`mod` mprod) . sum . map (\e → let m = mprod `div` modulo e in val e * m * modInv m (modulo e)) $ eqs
+  where
+    moduli = map modulo eqs
+    mprod = assert (all (≡ 1) [ gcd a b | a ← moduli, b ← moduli, a ≠ b ])
+                   (product moduli)
+
+modInv ∷ ∀ι. Integral ι ⇒ ι → ι → ι
+modInv a n = assert (g == 1) $ (x + n) `mod` n
+  where
+    (x, _, g) = egcd a n
+
+egcd :: ∀ι. Integral ι ⇒ ι -> ι -> (ι, ι, ι)
+egcd a 0 = (1, 0, a)
+egcd a b = let (q, r) = a `quotRem` b
+               (s, t, g) = egcd b r
+           in (t, s - q * t, g)
 
 main ∷ IO ()
 main = do
-    (from, buses) ← (read @Int *** map (read @Int) . filter (≠ "x") . splitsBy (≡ ',')) . break (≡ '\n') <$> getContents
+    (from, busesTS) ← (read @Integer *** map (second (read @Integer)) . filter ((≠ "x") . snd) . zip [0..] . splitsBy (≡ ',')) . break (≡ '\n') <$> getContents
+    let buses = map snd busesTS
     let dueIn x = x - (from `mod` x)
     let earliest = minimumBy (compare `on` dueIn) buses
     print (earliest * dueIn earliest)
+    let eqs = map (\(i, b) → ModEq (b - i) b) busesTS
+    print $ solve eqs
