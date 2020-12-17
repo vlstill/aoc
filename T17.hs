5195b29ab0e81add86763ca30af36d7be0b868d0
Vladimir Still <git@vstill.eu>
2020-12-17 10:35:29 +0100

    Second part

--- T17.hs
+++ T17.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE UnicodeSyntax, ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE UnicodeSyntax, ScopedTypeVariables, TypeApplications, DataKinds, KindSignatures #-}
 
 module T17 where
 
@@ -9,36 +9,53 @@ import Prelude.Unicode
 import Data.Tuple ( swap )
 import Data.List ( transpose, isPrefixOf )
 import Data.Foldable ( toList )
+import Data.Proxy
 import Control.Arrow
+
+import GHC.TypeLits
 
 import Data.Set ( Set, fromList, member )
 import qualified Data.Set as Set
 
-type PocketDimension = Set (Integer, Integer, Integer)
+newtype PocketDimension (δ ∷ Nat) = PD { unPD :: Set [Integer] }
 
-load ∷ String → PocketDimension
+load ∷ ∀δ. KnownNat δ ⇒ String → PocketDimension δ
 load = lines >>>
        map (zip [0..] >>> filter (snd >>> (≡ '#'))) >>>
        zip [0..] >>>
-       concatMap (\(x, l) -> map (\(y, _) -> (x, y, 0)) l) >>>
-       fromList
-
-step ∷ PocketDimension → PocketDimension
-step state = Set.filter active considered
+       concatMap (\(x, l) -> map (\(y, _) -> lift x y) l) >>>
+       fromList >>>
+       PD
   where
+    lift ∷ Integer → Integer → [Integer]
+    lift x y = take d ([x, y] <> repeat 0)
+
+    d = fromInteger $ natVal (Proxy @δ)
+
+step ∷ ∀δ. KnownNat δ ⇒ PocketDimension δ → PocketDimension δ
+step (PD state) = PD $ Set.filter active considered
+  where
+    d = fromInteger $ natVal (Proxy @δ)
     sigs = [-1, 0, 1]
-    considered = fromList [ (x + dx, y + dy, z + dz)
-                          | (x, y, z) ← toList state, dx ← sigs, dy ← sigs, dz ← sigs ]
-    active coo@(x, y, z)
+
+    diffs ∷ Int → [[Integer]]
+    diffs 1 = map (:[]) sigs
+    diffs n = (:) <$> sigs <*> diffs (n - 1)
+
+    considered = fromList [ zipWith (+) coo diff | coo ← toList state, diff ← diffs d ]
+
+    active coo
         | coo `member` state = activeNeighbours ∈ [2, 3]
         | otherwise          = activeNeighbours ≡ 3
       where
         activeNeighbours = count @Int (`member` state) neightbours
-        neightbours = [ (x + dx, y + dy, z + dz) | dx ← sigs, dy ← sigs, dz ← sigs,
-                                                   (dx, dy, dz) ≠ (0, 0, 0) ]
+        neightbours = [ zipWith (+) coo diff | diff ← diffs d, diff ≠ replicate d 0 ]
 
 main ∷ IO ()
 main = do
-    initial ← load <$> getContents
-    print . length $ fpow step 6 initial
+    initial ← getContents
+    let calc ∷ ∀δ. KnownNat δ ⇒ PocketDimension δ → IO ()
+        calc = print . length . unPD . fpow step 6
+    calc $ load @3 initial
+    calc $ load @4 initial
 
