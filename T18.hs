a977daee721e4d53634de83ec6c083d5bd45dbd8
Vladimir Still <git@vstill.eu>
2020-12-18 10:13:44 +0100

    A proper solution

--- T18.hs
+++ T18.hs
@@ -1,13 +1,26 @@
+{-# LANGUAGE LambdaCase, UnicodeSyntax #-}
+
 module T18 where
 
-(⊕) = (+)
-infixl 9 ⊕
-(⊛) = (*)
-infixl 8 ⊛
+import Utils
+import Data.List
+import Language.Haskell.Interpreter
 
-input = [
-    {- ... -}
-    ]
+run ∷ String → IO Integer
+run code = runInterpreter (setImports ["Prelude"] >> interpret code (1 :: Integer)) >>= \case
+                    Right x → pure x
+                    Left er → fail $ show er
 
+subst ∷ Char → Char
+subst '+' = '⊕'
+subst '*' = '⊛'
+subst x   = x
+
+addDefs ∷ String → Int → String
+addDefs code prioPlus = "let (⊛) = (*); infixl 8 ⊛; (⊕) = (+); infixl " ++ show prioPlus ++ " ⊕ in " ++ code
+
+main ∷ IO ()
 main = do
-    print $ sum input
+    expr ← intercalate " + " . map (embrace . map subst) . lines <$> getContents
+    print =<< run (addDefs expr 8)
+    print =<< run (addDefs expr 9)
