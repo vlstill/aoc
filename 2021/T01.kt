4a45a38735cad22e05c8a73a2a583a2ad7f3d87c
Vladimir Still <git@vstill.eu>
2021-12-01 18:35:29 +0100

    T01: Make it nicer

--- 2021/T01.kt
+++ 2021/T01.kt
@@ -1,21 +1,16 @@
 fun windowed_increases(data: List<Int>, window: Int): Int {
     var last: Int? = null
     var count = 0
-    for (w in data.windowed(size = window)) {
-        if (last != null && last!! < w.sum())
+    for (w in data.windowed(size = window).map { x -> x.sum() }) {
+        if (last != null && last < w)
             ++count
-        last = w.sum()
+        last = w
     }
     return count
 }
 
 fun main() {
-    var radar = ArrayList<Int>();
-    var sweep: Int? = readLine()?.toInt()
-    while (sweep != null) {
-        radar.add(sweep);
-        sweep = readLine()?.toInt()
-    }
+    var radar = generateSequence(::readLine).map(String::toInt).toList()
     println(windowed_increases(radar, 1))
     println(windowed_increases(radar, 3))
 }
