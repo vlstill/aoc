13b0370ae3f3643c151444b503a4a9dd7f32de83
Vladimir Still <git@vstill.eu>
2021-12-01 11:16:58 +0100

    T01: Rework for task 2

--- 2021/T01.kt
+++ 2021/T01.kt
@@ -1,13 +1,21 @@
+fun windowed_increases(data: List<Int>, window: Int): Int {
+    var last: Int? = null
+    var count = 0
+    for (w in data.windowed(size = window)) {
+        if (last != null && last!! < w.sum())
+            ++count
+        last = w.sum()
+    }
+    return count
+}
+
 fun main() {
-    var last: Int? = readLine()?.toInt()
-    var increases = 0
-    do {
-        val now: Int? = readLine()?.toInt()
-        if (now == null)
-            break;
-        if (now > last!!)
-            increases++
-        last = now
-    } while (true);
-    println(increases)
+    var radar = ArrayList<Int>();
+    var sweep: Int? = readLine()?.toInt()
+    while (sweep != null) {
+        radar.add(sweep);
+        sweep = readLine()?.toInt()
+    }
+    println(windowed_increases(radar, 1))
+    println(windowed_increases(radar, 3))
 }
