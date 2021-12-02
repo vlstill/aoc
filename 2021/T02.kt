5bf5890aa825ab7ad3de07812a1ce1ac65b23dfa
Vladimir Still <git@vstill.eu>
2021-12-02 10:22:37 +0100

    T02: Deduplicate & use when

--- 2021/T02.kt
+++ 2021/T02.kt
@@ -1,31 +1,17 @@
-fun get_final_position(input: List<Pair<String, Int>>): Pair<Int, Int> {
-    var depth = 0
-    var forward = 0
-    for ((com, arg) in input) {
-	if (com == "forward")
-	    forward += arg
-	if (com == "down")
-	    depth += arg
-	if (com == "up")
-	    depth -= arg
-    }
-    return Pair(forward, depth)
-}
-
-fun get_final_position_aim(input: List<Pair<String, Int>>): Pair<Int, Int> {
+fun get_final_position(input: List<Pair<String, Int>>, use_aim: Boolean): Pair<Int, Int> {
     var depth = 0
     var forward = 0
     var aim = 0
     for ((com, arg) in input) {
-	if (com == "forward") {
-	    forward += arg
-	    depth += aim * arg
-	}
-	if (com == "down")
-	    aim += arg
-	if (com == "up")
-	    aim -= arg
-	// println("$depth $forward $aim")
+        when (com) {
+            "forward" -> {
+                forward += arg
+                if (use_aim)
+                    depth += aim * arg
+            }
+            "down" -> if (use_aim) aim += arg else depth += arg
+            "up" ->   if (use_aim) aim -= arg else depth -= arg
+        }
     }
     return Pair(forward, depth)
 }
@@ -35,10 +21,11 @@ fun main() {
 		     .map { x -> x.split(" ") }
 		     .map { xs -> Pair(xs[0], xs[1].toInt()) }
 		     .toList()
-    val pos1 = get_final_position(input)
-    println(pos1)
+    val pos1 = get_final_position(input, false)
+    System.err.println(pos1)
     println(pos1.first * pos1.second)
-    val pos2 = get_final_position_aim(input)
-    println(pos2)
+
+    val pos2 = get_final_position(input, true)
+    System.err.println(pos2)
     println(pos2.first * pos2.second)
 }
