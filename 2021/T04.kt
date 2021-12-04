e952a6f53e6be550e9291155b84b060d4ff072ff
Vladimir Still <git@vstill.eu>
2021-12-04 07:46:32 +0100

    T04: Both parts

---
+++ 2021/T04.kt
@@ -0,0 +1,66 @@
+fun Boolean.toInt() = if (this) 1 else 0
+
+fun getBoard(): List<List<Int>>? {
+    var is_fist = true
+    var board = ArrayList<List<Int>>()
+    while (true) {
+        val line = readLine()
+        if (line == null && is_fist)
+            return null
+        if (line == null || line == "")
+            return board
+        val row = line.split(" ").filter { x -> x.length > 0 }.map(String::toInt).toList()
+        board.add(row)
+        is_fist = false
+    }
+}
+
+fun place(board: MutableList<MutableList<Int?>>, number: Int): Boolean {
+    val line_w = board[0].size
+    for (i in 0..board.size - 1) {
+        for (j in 0..line_w - 1) {
+            if (board[i][j] == number)
+                board[i][j] = null
+        }
+    }
+
+    return (board.any { r -> r.filter { x -> x != null }.toList().size == 0 }
+        || (0..line_w - 1).map { j -> (0..board.size - 1).map{ i -> board[i][j] }.toList() }
+              .any { r: List<Int?> -> r.filter { x -> x != null }.toList().size == 0 })
+}
+
+fun score(n: Int, board: List<List<Int?>>): Int {
+    return n * board.map { r -> r.filter { x -> x != null }.map { x -> x!! }.sum() }.sum()
+}
+
+fun play(boards_: List<List<List<Int?>>>, numbers: List<Int>, to_loose: Boolean): Int? {
+    var boards: MutableList<MutableList<MutableList<Int?>>?> = boards_.map { b -> b.map { r -> r.toMutableList() }.toMutableList() }.toMutableList()
+    var won = 0
+    for (n in numbers) {
+        for (i in 0..boards.size - 1) {
+            val board = boards[i]
+            if (board != null && place(board, n)) {
+                if (!to_loose)
+                    return n * board.map { r -> r.filter { x -> x != null }.map { x -> x!! }.sum() }.sum()
+                else {
+                    won++
+                    if (won == boards.size) {
+                        return n * board.map { r -> r.filter { x -> x != null }.map { x -> x!! }.sum() }.sum()
+                    }
+                }
+                boards[i] = null
+            }
+        }
+    }
+    return null
+}
+
+fun main() {
+    val numbers = readLine()!!.split(",").map(String::toInt).toList()
+    readLine()
+
+    val boards = generateSequence(::getBoard).toList()
+
+    println(play(boards, numbers, false)!!)
+    println(play(boards, numbers, true)!!)
+}
