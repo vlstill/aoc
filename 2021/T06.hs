17e8a6601ba05994f4b76d8a321b52b6ce5ac54e
Vladimir Still <git@vstill.eu>
2021-12-06 06:40:38 +0100

    T06: Both fast

--- 2021/T06.hs
+++ 2021/T06.hs
@@ -1,26 +1,32 @@
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeApplications, BangPatterns #-}
 
 module Main where
 
 import Control.Arrow
 import Data.List
 import Data.Monoid
+import Data.Function ( on )
 
 main = do
     input <- map toCnt . group . sort .  map (read @Integer) . words . map c2s <$> getContents
-    let res = map (foldMap (Sum . snd)) $ process input
-    print (take 80 res)
-    print (take 256 res)
+    let res x = sum . map snd . (!! x) $ process input
+    -- print (last . take 18 $ process input)
+    -- print (res 18)
+    print (res 80)
+    print (res 256)
   where
     c2s ',' = ' '
     c2s x   = x
     toCnt xs@(x:_) = (x, genericLength xs)
 
 process :: [(Integer, Integer)] -> [[(Integer, Integer)]]
-process = tail . iterate produce
+process = iterate produce
 
 produce :: [(Integer, Integer)] -> [(Integer, Integer)]
-produce = map (first (subtract 1)) >>> concatMap spawn
+produce !x = collect (concatMap (spawn . first (subtract 1)) x)
   where
-    spawn (-1, cnt) = [(6, cnt), (8, cnt)]
-    spawn x    = [x]
+    spawn (-1, !cnt) = [(6, cnt), (8, cnt)]
+    spawn x          = [x]
+
+    collect :: [(Integer, Integer)] -> [(Integer, Integer)]
+    collect = sort >>> groupBy ((==) `on` fst) >>> map (\xs@((x, _):_) -> (x, sum (map snd xs)))
