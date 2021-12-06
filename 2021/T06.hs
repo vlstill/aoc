7ee371536a1732ccbb38d5fd2adc615235616c21
Vladimir Still <git@vstill.eu>
2021-12-06 06:23:11 +0100

    T06: Part II slow

--- 2021/T06.hs
+++ 2021/T06.hs
@@ -3,19 +3,24 @@
 module Main where
 
 import Control.Arrow
+import Data.List
+import Data.Monoid
 
 main = do
-    input <- map (read @Integer) . words . map c2s <$> getContents
-    print (take 80 . map length $ process input)
+    input <- map toCnt . group . sort .  map (read @Integer) . words . map c2s <$> getContents
+    let res = map (foldMap (Sum . snd)) $ process input
+    print (take 80 res)
+    print (take 256 res)
   where
     c2s ',' = ' '
     c2s x   = x
+    toCnt xs@(x:_) = (x, genericLength xs)
 
-process :: [Integer] -> [[Integer]]
+process :: [(Integer, Integer)] -> [[(Integer, Integer)]]
 process = tail . iterate produce
 
-produce :: [Integer] -> [Integer]
-produce = map (subtract 1) >>> concatMap spawn
+produce :: [(Integer, Integer)] -> [(Integer, Integer)]
+produce = map (first (subtract 1)) >>> concatMap spawn
   where
-    spawn (-1) = [6, 8]
+    spawn (-1, cnt) = [(6, cnt), (8, cnt)]
     spawn x    = [x]
