aa81b7ee71fbe1ad1e8edd064e30a901bcd7bd7e
Vladimir Still <git@vstill.eu>
2021-12-09 06:35:54 +0100

    T09: Fist part

---
+++ 2021/T09.cpp
@@ -0,0 +1,76 @@
+#include <cassert>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <vector>
+
+struct Map {
+
+    Map(std::istream &is) {
+        std::string line;
+        while (std::getline(is, line)) {
+            assert(rowlength == 0 || rowlength == line.size());
+            rowlength = line.size();
+
+            for (char c : line) {
+                map.push_back(c - '0');
+            }
+        }
+    }
+
+    int operator[](std::pair<int, int> idx) const {
+        return map[idx.second * rowlength + idx.first];
+    }
+
+    template< typename Yield >
+    void neighbors(int x, int y, Yield yield) const {
+        for (int dx : { -1, 0, 1 }) {
+            for (int dy : { -1, 0, 1 }) {
+                if (std::abs(dx) + std::abs(dy) == 1) {
+                    int nx = x + dx;
+                    int ny = y + dy;
+                    if (0 <= nx && nx < cols() && 0 <= ny && ny < rows())
+                        yield((*this)[{nx, ny}], nx, ny);
+                }
+            }
+        }
+    }
+
+    template< typename Yield >
+    void enumerate(Yield yield) const {
+        for (int i = 0, s = map.size(); i < s; ++i) {
+            yield(map[i], i % rowlength, i / rowlength);
+        }
+    }
+
+    int rows() const { return map.size() / rowlength; }
+    int cols() const { return rowlength; }
+
+    std::vector<int> map;
+    size_t rowlength = 0;
+};
+
+int main() {
+    Map map(std::cin);
+
+    long risk = 0;
+    long basin_size = 1;
+    map.enumerate([&](auto val, auto x, auto y) {
+        bool is_min = true;
+        std::cerr << "lp" << val << " at " << x << " " << y;
+        map.neighbors(x, y, [&](auto nval, auto nx, auto ny) {
+            std::cerr << " [" << nx << ", " << ny << "]";
+            is_min = is_min && nval > val;
+        });
+        if (is_min) {
+            std::cerr << " TAKEN";
+            risk += val + 1;
+        }
+        std::cerr << '\n';
+    });
+    std::cout << risk << '\n';
+}
