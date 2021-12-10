ba2eb6bce83dc9164724848e4aab5070e03b9605
Vladimir Still <git@vstill.eu>
2021-12-10 06:16:21 +0100

    T10: Polishing

--- 2021/T10.cpp
+++ 2021/T10.cpp
@@ -14,19 +14,19 @@ std::map< char, char > close = { {'(', ')'},
                                  {'<', '>'} };
 
 
-std::map< char, int > points = { {')', 3 },
-                                 {']', 57 },
-                                 {'}', 1197 },
-                                 {'>', 25137 } };
-std::map< char, int > points2 = { {')', 1 },
-                                 {']', 2 },
-                                 {'}', 3 },
-                                 {'>', 4 } };
+std::map< char, int > points_err = { {')', 3 },
+                                     {']', 57 },
+                                     {'}', 1197 },
+                                     {'>', 25137 } };
+std::map< char, int > points_complete = { {')', 1 },
+                                          {']', 2 },
+                                          {'}', 3 },
+                                          {'>', 4 } };
 
 int main() {
     std::string line;
-    long score = 0;
-    std::vector< long > scores;
+    long score_err = 0;
+    std::vector< long > scores_complete;
     while (std::getline(std::cin, line)) {
         std::vector< char > stack;
         bool corrupted = false;
@@ -38,7 +38,7 @@ int main() {
                 char top = stack.back();
                 stack.pop_back();
                 if (top != c) {
-                    score += points[c];
+                    score_err += points_err[c];
                     corrupted = true;
                     break;
                 }
@@ -48,12 +48,12 @@ int main() {
             long score_complete = 0;
             for (auto it = stack.rbegin(), e = stack.rend(); it != e; ++it) {
                 score_complete *= 5;
-                score_complete += points2[*it];
+                score_complete += points_complete[*it];
             }
-            scores.push_back(score_complete);
+            scores_complete.push_back(score_complete);
         }
     }
-    std::cout << score << '\n';
-    std::sort(scores.begin(), scores.end());
-    std::cout << scores[scores.size() / 2] << '\n';
+    std::cout << score_err << '\n';
+    std::sort(scores_complete.begin(), scores_complete.end());
+    std::cout << scores_complete[scores_complete.size() / 2] << '\n';
 }
