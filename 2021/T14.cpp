f1c4ab69d7e0789204236024dcf7508cd1a42092
Vladimir Still <git@vstill.eu>
2021-12-14 07:03:15 +0100

    T14: Part two fast

--- 2021/T14.cpp
+++ 2021/T14.cpp
@@ -14,12 +14,37 @@
 #include <list>
 #include <limits>
 
-void step(std::list<char> &polymer, const std::map<std::string, char> inserts) {
-    for (auto it = polymer.begin(), e = std::prev(polymer.end()); it != e; ++it) {
-	std::string key{*it, *std::next(it)};
-	if (auto found = inserts.find(key); found != inserts.end())
-	    it = polymer.insert(std::next(it), found->second);
+using PolyPairs = std::map<std::string, long>;
+
+
+PolyPairs step(const PolyPairs &polypairs, const std::map<std::string, char> inserts) {
+    PolyPairs copy(polypairs);
+    for (auto &[pair, cnt] : polypairs) {
+	if (auto found = inserts.find(pair); found != inserts.end()) {
+	    auto c = found->second;
+	    copy[pair] -= cnt;
+	    std::string fst{pair[0], c}, snd{c, pair[1]};
+	    copy[fst] += cnt;
+	    copy[snd] += cnt;
+	}
     }
+    /*
+    std::vector<std::string> drop;
+    for (auto &[p, cnt] : polypairs) {
+	if (cnt == 0)
+	    drop.push_back(p);
+    }
+    for (auto &p : drop)
+	copy.erase(p);
+	*/
+    return copy;
+}
+
+void dump(PolyPairs &pp) {
+    for (auto &[p, c] : pp) {
+	std::cerr << p << c;
+    }
+    std::cerr << '\n';
 }
 
 int main() {
@@ -39,13 +64,24 @@ int main() {
     for (auto [k, v] : inserts)
 	std::cerr << k << " → " << v << '\n';
 
-    std::list polymer(templ.begin(), templ.end());
-    for (int i = 1; i <= 10; ++i) {
-	step(polymer, inserts);
+    std::map<std::string, long> polypairs;
+    for (auto it = templ.begin(), e = std::prev(templ.end()); it != e; ++it) {
+	polypairs[std::string{*it, *std::next(it)}] = 1;
     }
+
+    dump(polypairs);
+    for (int i = 1; i <= 40; ++i) {
+	polypairs = step(polypairs, inserts);
+	std::cerr << i << '.'; dump(polypairs);
+    }
+
     std::map<char, long> counts;
-    for (auto c : polymer)
-	++counts[c];
+    for (auto [k, v] : polypairs) {
+	counts[k[0]] += v;
+	counts[k[1]] += v;
+    }
+    counts[templ.front()]++;
+    counts[templ.back()]++;
 
     {
 	long min = std::numeric_limits<long>::max(), max = 0;
@@ -55,9 +91,18 @@ int main() {
 	    std::cerr << k << " = " << v << '\n';
 	}
 
-	std::cout << max - min << '\n';
+	std::cout << max / 2 - min / 2 << '\n';
     }
 
+    // N B C C N B B B C B H C B
+    // NBBBCNCCNBBNBNBBCHBHHBCHB
+    // N B B B C N C C N B B N B N B B C H B H H B C H B
+    // NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB
+    // --B****BCCNBCNCC--B**B--BB--B**BCBHCBHHNHCBBCBHCB
+
+}
+
+/*
     for (int i = 1; i <= 30; ++i) {
 	step(polymer, inserts);
     }
@@ -71,3 +116,4 @@ int main() {
 	std::cout << max - min << '\n';
     }
 }
+*/
