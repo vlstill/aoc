8752b72ba087bd714cd60e9c32a5ac3aee0d7cb5
Vladimir Still <git@vstill.eu>
2021-12-17 07:23:24 +0100

    T17: Bounds, cleanup

--- 2021/T17.cpp
+++ 2021/T17.cpp
@@ -35,12 +35,13 @@ int main() {
     };
     std::tie(x_min, x_max) = parse_range();
     std::tie(y_min, y_max) = parse_range();
+    assert(x_min <= x_max);
+    assert(y_min <= y_max);
 
     auto go = [=](int xv, int yv) -> std::optional<int> {
 	int top = 0;
 	int x = 0, y = 0;
 
-//	std::cerr << xv << ',' << yv << ": 0,0 → ";
 	for (int i = 0; ; ++i) {
 	    x += xv;
 	    xv = std::max(0, xv - 1);
@@ -48,34 +49,29 @@ int main() {
 	    --yv;
 	    top = std::max(top, y);
 
-//	    std::cerr << x << ',' << y;
 
 	    if (x_min <= x && x <= x_max && y_min <= y && y <= y_max) {
-//		std::cerr << " ⊤\n";
 		return top;
 	    }
 	    if (x > x_max || y < y_min) {
-//		std::cerr << " ⊥\n";
 		return std::nullopt;
 	    }
 
-//	    std::cerr << " → ";
 	}
     };
 
     std::set<std::pair<int, std::pair<int, int>>> reach;
 
     // assuming the target is in x positive, y negative direct heading from 0,0
+    assert(x_min > 0);
+    assert(y_max < 0);
     for (int xv = 0; xv <= x_max; ++xv) {
-	for (int yv = y_min; yv < 10000; ++yv) {
+	for (int yv = y_min; yv < -y_min; ++yv) {
 	    auto r = go(xv, yv);
 	    if (r.has_value())
 		reach.insert({*r, {xv, yv}});
 	}
     }
-//    for (auto [h, p] : reach) {
-//	std::cerr << h << ' ' << p.first << ',' << p.second << '\n';
-//    }
     std::cout << reach.rbegin()->first << '\n';
     std::cout << reach.size() << '\n';
 }
