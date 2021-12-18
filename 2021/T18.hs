c45b1b350718e454f6818fd80d2915f578bd46a1
Vladimir Still <git@vstill.eu>
2021-12-18 09:56:21 +0100

    T18: Cleanup

--- 2021/T18.hs
+++ 2021/T18.hs
@@ -1,11 +1,6 @@
 {-# LANGUAGE TypeApplications, Strict #-}
 module T18 where
 
-import System.IO.Unsafe ( unsafePerformIO )
-
-uprint :: Show a => a -> ()
-uprint = unsafePerformIO . print
-
 data Tree = Leaf Int
           | Node Tree Tree
           deriving (Show, Read, Eq)
@@ -34,7 +29,7 @@ goR (TZip back (Node l r)) = TZip (L l : back) r
 
 onNextL :: (TZip -> TZip) -> TZip -> TZip
 onNextL f z@(TZip (L _:_) _) = goR (goUp (onRightmost f (goL (goUp z))))
-onNextL f z@(TZip []    _) = z -- nothing
+onNextL f z@(TZip []    _) = z -- do nothing
 onNextL f z@(TZip (R _:_) _) = goL (onNextL f (goUp z))
 
 onRightmost :: (TZip -> TZip) -> TZip -> TZip
@@ -43,7 +38,7 @@ onRightmost f z                      = goUp (onRightmost f (goR z))
 
 onNextR :: (TZip -> TZip) -> TZip -> TZip
 onNextR f z@(TZip (R _:_) _) = goL (goUp (onLeftmost f (goR (goUp z))))
-onNextR f z@(TZip []    _) = z -- nothing
+onNextR f z@(TZip []    _) = z -- do nothing
 onNextR f z@(TZip (L _:_) _) = goR (onNextR f (goUp z))
 
 onLeftmost :: (TZip -> TZip) -> TZip -> TZip
@@ -56,9 +51,9 @@ goUp (TZip (R r:back) l) = TZip back (Node l r)
 set t (TZip back _) = TZip back t
 
 reduce :: Tree -> Tree
-reduce t = uprint ("reduce", reprt t) `seq` case red (zipper t) of
+reduce t = case red (zipper t) of
               Left t'  -> reduce (unzipper t')
-              Right t' -> uprint ("done", repr t') `seq` unzipper t'
+              Right t' -> unzipper t'
   where
     red z = tryExplode z >> trySplit z
 
@@ -75,33 +70,13 @@ tryExplode z@(TZip _ (Node _ _))
 tryExplode z@(TZip _ (Leaf _)) = Right z
 
 explode :: TZip -> TZip
-explode z@(TZip _ (Node (Leaf l) (Leaf r))) = {- onNextR fix $ onNextL fix $ -} set (Leaf 0) $ onNextR (add r) $ onNextL (add l) z
-explode z = error $ "explode " ++ show z
+explode z@(TZip _ (Node (Leaf l) (Leaf r))) = set (Leaf 0) $ onNextR (add r) $ onNextL (add l) z
 
 add :: Int -> TZip -> TZip
 add val (TZip back (Leaf v)) = TZip back (Leaf (v + val))
 
-{-
-fix :: TZip -> TZip
-fix r@(TZip _ (Leaf v))
-  | otherwise = r
-  | v >= 10   = uprint ("explode -> split", repr r) `seq` split r 
- -}
 split :: TZip -> TZip
-split (TZip back (Leaf v)) = {- let r = -} TZip back (Node (Leaf (v `div` 2)) (Leaf ((v + 1) `div` 2)))
-{-
-                             in if False && depth r >= 4 then uprint ("split -> explode", repr r) `seq` explode r
-                                                else r
- -}
-emap :: (a -> b) -> Either a a -> Either b b
-emap f (Left x) = Left (f x)
-emap f (Right x) = Right (f x)
-
-repr :: TZip -> String
-repr z@(TZip _ top) = take 30 (show top ++ replicate 30 ' ') ++ reprt (unzipper z)
-
-reprt (Leaf x) = show x
-reprt (Node x y) = "(" ++ reprt x ++ ", " ++ reprt y ++ ")"
+split (TZip back (Leaf v)) = TZip back (Node (Leaf (v `div` 2)) (Leaf ((v + 1) `div` 2)))
 
 main = do
     numbers <- fmap (read @Tree) . lines <$> getContents
