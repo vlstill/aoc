94d1f1d3d7aec315a5f13bf0861cea1ec8c388e8
Vladimir Still <git@vstill.eu>
2021-12-23 08:22:59 +0100

    T23: Part one

---
+++ 2021/T23.cpp
@@ -0,0 +1,183 @@
+#include <cassert>
+#include <algorithm>
+#include <numeric>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <deque>
+#include <optional>
+#include <list>
+#include <limits>
+#include <queue>
+#include <iomanip>
+#include <memory>
+#include <variant>
+#include <type_traits>
+#include <cmath>
+
+int move_energy(char a) {
+    return std::pow(10, a - 'A');
+}
+
+struct Antipodium {
+    std::array<char, 11> hallway{};
+    std::array<std::array<char, 2>, 4> burrows{};
+
+    auto operator<=>(const Antipodium &) const = default;
+
+    int dst_burrow_door(char a) {
+	return 2 + 2 * (a - 'A');
+    }
+
+    int dst_burrow_idx(char a) {
+	return a - 'A';
+    }
+
+    bool sorted() {
+	for (auto v : hallway) {
+	    if (v != 0)
+		return false;
+	}
+	for (int i = 0; i < ssize(burrows); ++i) {
+	    for (auto a : burrows[i])
+		if (i != dst_burrow_idx(a))
+		    return false;
+	}
+	return true;
+    }
+
+    std::optional<std::pair<Antipodium, long>> go_home(char who, int from_where) {
+        int door = dst_burrow_door(who);
+        int where = who - 'A';
+        for (auto a : burrows[where])
+            if (a != 0 && a != who)
+                return std::nullopt;
+        int dir = door > from_where ? 1 : -1;
+        for (int i = from_where + dir; i != door + dir; i += dir) {
+            if (hallway[i] != 0)
+                return std::nullopt;
+        }
+        auto copy(*this);
+        copy.hallway[from_where] = 0;
+        for (int i : {1, 0}) {
+            if (copy.burrows[where][i] == 0) {
+                copy.burrows[where][i] = who;
+                return std::pair(copy, (i + 1 + std::abs(from_where - door)) * move_energy(who));
+            }
+        }
+        __builtin_trap();
+    }
+
+    void go_out(char who, int bur, int idx, auto yield) {
+        if (idx == 1 && burrows[bur][0] != 0)
+            return;
+        auto pos = 2 + 2 * bur;
+        for (auto dir : {1, -1}) {
+            for (int i = pos + dir; i >= 0 && i < ssize(hallway); i += dir) {
+                if (i == 2 || i == 4 || i == 6 || i == 8)
+                    continue;
+                if (hallway[i] != 0)
+                    break;
+                auto copy(*this);
+                copy.burrows[bur][idx] = 0;
+                copy.hallway[i] = who;
+                yield(copy, move_energy(who) * (idx + 1 + std::abs(i - pos)));
+            }
+        }
+    }
+
+    void dump(int depth) {
+        std::string pad(depth * 4, ' ');
+        std::cerr << pad << std::string(13, '#') << '\n';
+        std::cerr << pad << '#';
+        for (auto v : hallway)
+            std::cerr << (v ?: '.');
+        std::cerr << "#\n";
+        std::cerr << pad << "###";
+        auto b = [&](int i) {
+            for (auto bur : burrows)
+                std::cerr << (bur[i] ?: '.') << '#';
+        };
+        b(0);
+        std::cerr << "##\n" << pad << "  #";
+        b(1);
+        std::cerr << '\n' << pad << "  " << std::string(9, '#') << '\n';
+    }
+};
+
+std::optional<long> solve(Antipodium a, long current_energy, long current_max, auto &seen, int depth = 0) {
+//    std::cerr << 's' << depth << '\n';
+    std::cerr << std::string(depth * 4, ' ') << current_energy << ' ' << current_max << '\n';
+    a.dump(depth);
+    if (current_energy > current_max)
+	return std::nullopt;
+
+    if (auto it = seen.find(a); it != seen.end() && it->second <= current_energy)
+        return std::nullopt;
+
+    seen[a] = current_energy;
+
+    if (a.sorted()) {
+        std::cerr << "sorted " << current_energy << '\n';
+	return current_energy;
+    }
+
+    long local_best = current_max;
+    int local_solved = 0;
+    for (int i = 0; i < ssize(a.hallway); ++i) {
+	if (a.hallway[i] != 0) {
+            std::cerr << std::string(4 * depth, ' ') << a.hallway[i] << " in\n";
+            if (auto r = a.go_home(a.hallway[i], i)) {
+		auto [next, price] = *r;
+                if (auto s = solve(next, current_energy + price, local_best, seen, depth + 1)) {
+                    ++local_solved;
+                    local_best = std::min(local_best, *s);
+                }
+            }
+	}
+    }
+    for (int b = 0; b < 4; ++b) {
+        for (int i : {0, 1}) {
+            auto antipod = a.burrows[b][i];
+            if (antipod != 0 && (b != a.dst_burrow_idx(antipod) || (i == 0 && a.dst_burrow_idx(a.burrows[b][1] != b)))) {
+                std::cerr << std::string(4 * depth, ' ') << antipod << b << " out\n";
+                a.go_out(antipod, b, i, [&](auto next, auto price) {
+                    if (auto s = solve(next, current_energy + price, local_best, seen, depth + 1)) {
+                        ++local_solved;
+                        local_best = std::min(local_best, *s);
+                    }
+                });
+            }
+        }
+    }
+    return local_solved ? std::optional(local_best) : std::nullopt;
+}
+
+int main() {
+    Antipodium input;
+    {
+	std::string line;
+	std::getline(std::cin, line);
+	std::getline(std::cin, line);
+	std::string burrows[2];
+	std::getline(std::cin, burrows[0]);
+	std::getline(std::cin, burrows[1]);
+	int j = 0;
+	for (auto &bur : burrows) {
+	    for (int i = 0; i < 4; ++i) {
+		input.burrows[i][j] = bur[3 + 2 * i];
+	    }
+	    ++j;
+	}
+
+	input.dump(0);
+    }
+
+    std::map<Antipodium, long> seen;
+    std::cout << solve(input, 0, std::numeric_limits<long>::max(), seen).value();
+}
