ed48828d0d99d2485ea964c4fa38c56998174c0f
Vladimir Still <git@vstill.eu>
2022-12-03 06:19:04 +0100

    T03: Part II

--- 2022/T03.go
+++ 2022/T03.go
@@ -19,15 +19,30 @@ func decode(str string) (out []int) {
     return
 }
 
+func find_badge(elves [][]int) int {
+    for i := 0; i < len(elves[0]); i++ {
+        for j := 0; j < len(elves[1]); j++ {
+            for k := 0; k < len(elves[2]); k++ {
+                if elves[0][i] == elves[1][j] && elves[1][j] == elves[2][k] {
+                    return elves[0][i]
+                }
+            }
+        }
+    }
+    return 0
+}
+
 func main() {
     scanner := bufio.NewScanner(os.Stdin)
     score := 0
     score2 := 0
+    group := make([][]int, 0, 3)
     for scanner.Scan() {
         line := scanner.Text()
+        all := decode(line)
         first := decode(line[0:len(line)/2])
         second := decode(line[len(line)/2:])
-        fmt.Println(first, second)
+//        fmt.Println(first, second)
 
         found := false
         for i := 0; !found && i < len(first); i++ {
@@ -39,9 +54,11 @@ func main() {
             }
         }
 
-
-        score += 0
-        score2 += 0
+        group = append(group, all)
+        if len(group) == 3 {
+            score2 += find_badge(group)
+            group = make([][]int, 0, 3)
+        }
     }
     fmt.Println(score)
     fmt.Println(score2)
