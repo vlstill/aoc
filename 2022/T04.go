b11b6564d7f6c186d125259d37a599dfd1c027df
Vladimir Still <git@vstill.eu>
2022-12-04 06:09:56 +0100

    T04: Part I

---
+++ 2022/T04.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+    "strconv"
+)
+
+func rng(in string) (int, int) {
+    split := strings.Split(in, "-")
+    a, _ := strconv.Atoi(split[0])
+    b, _ := strconv.Atoi(split[1])
+    return a, b
+}
+
+func main() {
+    scanner := bufio.NewScanner(os.Stdin)
+    pt1 := 0
+    pt2 := 0
+    for scanner.Scan() {
+        line := scanner.Text()
+        split := strings.Split(line, ",")
+        a0, b0 := rng(split[0])
+        a1, b1 := rng(split[1])
+
+        if ((a0 <= a1 && b0 >= b1) || (a1 <= a0 && b1 >= b0)) {
+            pt1 += 1
+        } 
+    }
+    fmt.Println(pt1)
+    fmt.Println(pt2)
+}
+
+// vim: expandtab tw=99 colorcolumn=100
