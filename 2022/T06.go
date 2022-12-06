6b8cff4a4608300f7210a11fc03fc6a98e9e8863
Vladimir Still <git@vstill.eu>
2022-12-06 06:10:35 +0100

    T06: Part II, part I broken

--- 2022/T06.go
+++ 2022/T06.go
@@ -13,8 +13,10 @@ func main() {
     for scanner.Scan() {
         line := scanner.Text()
         buf := make([]rune, 4)
+        mebuf := make([]rune, 14)
         for i, v := range line {
             buf[i % 4] = v
+            mebuf[i % 14] = v
             same := false
             for j, a := range buf {
                 for k, b := range buf {
@@ -25,6 +27,17 @@ func main() {
             }
             if i >= 3 && !same {
                 pt1 += i + 1
+            }
+            sameme := false
+            for j, a := range mebuf {
+                for k, b := range mebuf {
+                    if j != k && a == b {
+                        sameme = true
+                    }
+                }
+            }
+            if i >= 13 && !sameme {
+                pt2 += i + 1
                 break
             }
         }
