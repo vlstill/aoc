58dc1e926a828725a913a0e0ca33ebd15f220422
Vladimir Still <git@vstill.eu>
2022-12-06 06:13:48 +0100

    T06: Both parts reasonable

--- 2022/T06.go
+++ 2022/T06.go
@@ -6,41 +6,33 @@ import (
     "os"
 )
 
+func scanline(line string, cnt int) int {
+    buf := make([]rune, cnt)
+    for i, v := range line {
+        buf[i % cnt] = v
+        same := false
+        for j, a := range buf {
+            for k, b := range buf {
+                if j != k && a == b {
+                    same = true
+                }
+            }
+        }
+        if i >= cnt - 1 && !same {
+            return i + 1
+        }
+    }
+    return 0
+}
+
 func main() {
     scanner := bufio.NewScanner(os.Stdin)
     pt1 := 0
     pt2 := 0
     for scanner.Scan() {
         line := scanner.Text()
-        buf := make([]rune, 4)
-        mebuf := make([]rune, 14)
-        for i, v := range line {
-            buf[i % 4] = v
-            mebuf[i % 14] = v
-            same := false
-            for j, a := range buf {
-                for k, b := range buf {
-                    if j != k && a == b {
-                        same = true
-                    }
-                }
-            }
-            if i >= 3 && !same {
-                pt1 += i + 1
-            }
-            sameme := false
-            for j, a := range mebuf {
-                for k, b := range mebuf {
-                    if j != k && a == b {
-                        sameme = true
-                    }
-                }
-            }
-            if i >= 13 && !sameme {
-                pt2 += i + 1
-                break
-            }
-        }
+        pt1 += scanline(line, 4)
+        pt2 += scanline(line, 14)
     }
     fmt.Println(pt1)
     fmt.Println(pt2)
