f8dfe5060b90ae8b6246ec2a36a4b2ab9ffe14c6
Vladimir Still <git@vstill.eu>
2022-12-07 06:58:49 +0100

    T07: Cleanup

--- 2022/T07.go
+++ 2022/T07.go
@@ -19,7 +19,6 @@ func main() {
         line := scanner.Text()
         split := strings.Split(line, " ")
         if (split[0] == "$" && split[1] == "cd") {
-            fmt.Println(dirsz)
             if split[2] == ".." {
                 path = path[0:len(path)-1]
             } else if split[2] == "/" {
@@ -27,28 +26,22 @@ func main() {
             } else {
                 path = append(path, split[2])
             }
-            fmt.Println(path)
-        } else if split[0] == "$" && split[1] == "ls" {
-            continue
         } else if split[0] != "dir" {
             sz, _ := strconv.Atoi(split[0])
-            fmt.Println(sz, split[1])
             for i := 0; i <= len(path); i++ {
                 dirsz[strings.Join(path[0:i], "/")] += sz
             }
         }        
     }
-    fmt.Println(dirsz)
     total := dirsz[""]
+    pt2 = total
     need := 30000000 - (70000000 - total)
     for _, v := range dirsz {
         if (v < 100000) {
             pt1 += v
         }
         if v > need {
-//            fmt.Println(v)
             pt2 = utils.Min(pt2, v)
-//            fmt.Println(v)
         }
     }
     fmt.Println(pt1)
