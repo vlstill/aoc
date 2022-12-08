600f6bbb951c1c5871b1a5d103ed5f813195d56b
Vladimir Still <git@vstill.eu>
2022-12-08 06:44:46 +0100

    Update outline

--- 2022/outline.go
+++ 2022/outline.go
@@ -4,20 +4,30 @@ import (
     "bufio"
     "fmt"
     "os"
+    "strings"
     "strconv"
     "aoc/utils"
 )
 
+func use(_ interface{}) {
+    if 4 == 2 {
+        _ = utils.Min(4, 2)
+        _ = strings.Split("", "")
+        _, _ = strconv.Atoi("42")
+    }
+}
+
 func main() {
     scanner := bufio.NewScanner(os.Stdin)
+    pt1 := 0
+    pt2 := 0
     for scanner.Scan() {
         line := scanner.Text()
-        val, _ := strconv.Atoi(line)
-
-
+        split := strings.Split(line, " ")
+        use(split)
     }
-    fmt.Println("a1")
-    fmt.Println("a2")
+    fmt.Println(pt1)
+    fmt.Println(pt2)
 }
 
 // vim: expandtab tw=99 colorcolumn=100
