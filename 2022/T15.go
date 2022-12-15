5a80c7e187b94639ca676b89b406aa927c64d94e
Vladimir Still <git@vstill.eu>
2022-12-15 07:39:33 +0100

    T15: Part II

--- 2022/T15.go
+++ 2022/T15.go
@@ -34,59 +34,76 @@ func dist(a, b Point) int {
     return utils.Abs(a.X - b.X) + utils.Abs(a.Y - b.Y)
 }
 
-func filled(s2b map[Point]Point, line int) int {
-    fill := make(utils.Set[int])
-    for sen, bea := range s2b {
-        d := dist(sen, bea)
-        may := utils.Max(sen.Y, bea.Y) + d
-        miy := utils.Min(sen.Y, bea.Y) - d
-        if !(miy <= line && line <= may) {
-            continue
-        }
-        for dx := -d; dx <= d; dx++ {
-            dy := line - sen.Y
-            if utils.Abs(dx) + utils.Abs(dy) > d {
-                continue
+type Interval struct {
+    From int
+    To int
+}
+
+type IntervalSet []Interval
+
+func MakeISet(from, to int) IntervalSet {
+    return IntervalSet([]Interval{Interval{from, to}})
+}
+
+func (self *IntervalSet) Remove(from, to int) {
+//    fmt.Println("remove", from, to)
+    out := IntervalSet([]Interval{})
+    for _, v := range *self {
+        if v.To < from {
+            out = append(out, v)
+//            fmt.Println("a")
+        } else if v.From > to {
+//            fmt.Println("b")
+            out = append(out, v)
+        } else if v.From >= from && v.To <= to {
+//            fmt.Println("c")
+            continue // skip
+        } else {
+            if from <= v.To && v.From <= from - 1 {
+                out = append(out, Interval{v.From, from - 1})
+//                fmt.Println("d", out)
             }
-            y := sen.Y + dy
-            x := sen.X + dx
-            if y == line && (x != bea.X || y != bea.Y) {
-                fill.Insert(x)
+            if to < v.To {
+                out = append(out, Interval{to + 1, v.To})
+//                fmt.Println("e", out)
             }
         }
     }
-    return len(fill)
+    *self = out
+}
+
+func (self IntervalSet) Size() int {
+    out := 0
+    for _, v := range self {
+        out += v.To - v.From + 1
+    }
+    return out
+}
+
+func filled(s2b map[Point]Point, line int, xbound int) IntervalSet {
+    fill := MakeISet(0, xbound)
+    for sen, bea := range s2b {
+        d := dist(sen, bea)
+        disy := utils.Abs(line - sen.Y)
+        if disy > d {
+            continue
+        }
+        disx := d - disy
+        fill.Remove(sen.X - disx, sen.X + disx)
+//        fmt.Println(fill)
+    }
+    return fill
 }
 
 func fill2D(s2b map[Point]Point, bound int) Point {
-    fill := utils.MakeMap(bound, bound, false)
-    for sen, bea := range s2b {
-        d := dist(sen, bea)
-        mix := utils.Min(sen.X, bea.X) - d
-        miy := utils.Min(sen.Y, bea.Y) - d
-        if miy > bound || mix > bound {
-            continue
+    for y := 0; y <= bound; y++ {
+        line := filled(s2b, y, bound)
+        if len(line) == 1 {
+            return Point{line[0].From, y}
         }
-        for dx := -d; dx <= d; dx++ {
-            for dy := -d; dy <= d; dy++ {
-                if utils.Abs(dx) + utils.Abs(dy) > d {
-                    continue
-                }
-                pt := Point{sen.X + dx, sen.Y + dy}
-                if fill.In(pt) {
-                    fill.Set(pt, true)
-                }
-            }
-        }
+        fmt.Print(".")
     }
-    for y, line := range fill {
-        for x, v := range line {
-            if !v {
-                return Point{x, y}
-            }
-        }
-    }
-    panic("not found")
+    panic("ei")
 }
 
 func freq(p Point) int {
@@ -105,13 +122,13 @@ func main() {
         map_[sensor] = 'S'
         map_[beacon] = 'B'
         s2b[sensor] = beacon
-        fmt.Println(sensor, beacon)
+//        fmt.Println(sensor, beacon)
         use(split)
     }
-    fmt.Println(s2b)
+//    fmt.Println(s2b)
 
-    fmt.Println(filled(s2b, 10))
-    fmt.Println(filled(s2b, 2000000))
+//    fmt.Println(1000000001 - filled(s2b, 10, 1000000000).Size())
+//    fmt.Println(filled(s2b, 2000000))
     fmt.Println(freq(fill2D(s2b, 4000000)))
 }
 
