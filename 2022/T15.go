31d460ef186278e6272291d31818b764eb7d7e23
Vladimir Still <git@vstill.eu>
2022-12-15 07:58:07 +0100

    T15: Both parts and some cleanup

--- 2022/T15.go
+++ 2022/T15.go
@@ -9,14 +9,6 @@ import (
     "aoc/utils"
 )
 
-func use(_ interface{}) {
-    if 4 == 2 {
-        _ = utils.Min(4, 2)
-        _ = strings.Split("", "")
-        _, _ = strconv.Atoi("42")
-    }
-}
-
 type Point = utils.Point
 
 func pasgn(asgn string) int {
@@ -46,26 +38,20 @@ func MakeISet(from, to int) IntervalSet {
 }
 
 func (self *IntervalSet) Remove(from, to int) {
-//    fmt.Println("remove", from, to)
     out := IntervalSet([]Interval{})
     for _, v := range *self {
         if v.To < from {
             out = append(out, v)
-//            fmt.Println("a")
         } else if v.From > to {
-//            fmt.Println("b")
             out = append(out, v)
         } else if v.From >= from && v.To <= to {
-//            fmt.Println("c")
             continue // skip
         } else {
             if from <= v.To && v.From <= from - 1 {
                 out = append(out, Interval{v.From, from - 1})
-//                fmt.Println("d", out)
             }
             if to < v.To {
                 out = append(out, Interval{to + 1, v.To})
-//                fmt.Println("e", out)
             }
         }
     }
@@ -80,8 +66,7 @@ func (self IntervalSet) Size() int {
     return out
 }
 
-func filled(s2b map[Point]Point, line int, xbound int) IntervalSet {
-    fill := MakeISet(0, xbound)
+func (fill *IntervalSet) Subtract(s2b map[Point]Point, line int) {
     for sen, bea := range s2b {
         d := dist(sen, bea)
         disy := utils.Abs(line - sen.Y)
@@ -90,20 +75,34 @@ func filled(s2b map[Point]Point, line int, xbound int) IntervalSet {
         }
         disx := d - disy
         fill.Remove(sen.X - disx, sen.X + disx)
-//        fmt.Println(fill)
     }
-    return fill
 }
 
 func fill2D(s2b map[Point]Point, bound int) Point {
     for y := 0; y <= bound; y++ {
-        line := filled(s2b, y, bound)
+        line := MakeISet(0, bound)
+        line.Subtract(s2b, y)
         if len(line) == 1 {
             return Point{line[0].From, y}
         }
-        fmt.Print(".")
     }
     panic("ei")
+}
+
+func part1(s2b map[Point]Point, line int) int {
+    line1 := MakeISet(-1000000000, 1000000000)
+    base := line1.Size()
+    line1.Subtract(s2b, 2000000)
+    devices := make(utils.Set[Point])
+    for a, b := range s2b {
+        if a.Y == line {
+            devices.Insert(a)
+        }
+        if b.Y == line {
+            devices.Insert(b)
+        }
+    }
+    return base - line1.Size() - len(devices)
 }
 
 func freq(p Point) int {
@@ -122,13 +121,9 @@ func main() {
         map_[sensor] = 'S'
         map_[beacon] = 'B'
         s2b[sensor] = beacon
-//        fmt.Println(sensor, beacon)
-        use(split)
     }
-//    fmt.Println(s2b)
 
-//    fmt.Println(1000000001 - filled(s2b, 10, 1000000000).Size())
-//    fmt.Println(filled(s2b, 2000000))
+    fmt.Println(part1(s2b, 2000000))
     fmt.Println(freq(fill2D(s2b, 4000000)))
 }
 
